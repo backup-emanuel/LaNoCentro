de56066ce186799209b2266ff3d5365980d465b1
CleoniceSoares <35998428+CleoniceSoares@users.noreply.github.com>
2020-04-19 11:05:17 -0300

    Atualização - Adicionando a entidade cidades

---
+++ ServicoApp/models/cidade.py
@@ -0,0 +1,25 @@
+from common.database import db
+from flask_restful import fields
+from sqlalchemy.ext.orderinglist import ordering_list
+
+
+cidade_campos = {
+    'id': fields.Integer(attribute='id'),
+    'nome': fields.String(attribute='nome'),
+}
+
+
+'''
+    Classe Cidade.
+'''
+class CidadeModel(db.Model):
+    __tablename__ = 'tb_cidade'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255))
+
+    def __init__(self, nome):
+        self.cidade = cidade
+
+    def __str__(self):
+        return '<Cidade %s>'%(self.cidade)
